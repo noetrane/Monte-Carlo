b426e767ce4ff4c361ccbb291344e8213845b157
Noé Trane <60549655+noetrane@users.noreply.github.com>
2025-09-02 23:34:51 +0100

    Update MonteCarloSimulation.py

--- MonteCarloSimulation.py
+++ MonteCarloSimulation.py
@@ -5,21 +5,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+#start timer to measure time taken to estimate pi
 start = time.perf_counter()
 
 def estimate_pi(num_points):
 
+    # define arrays
     x_inside, y_inside = [], []
     x_outside, y_outside = [], []
     circle_points = 0
     points = []
 
+    # draw the circle of radius 1
     theta = np.linspace(0, 2*np.pi, 500)
     x_circle = np.cos(theta)
     y_circle = np.sin(theta)
     plt.plot(x_circle, y_circle, color="black", linewidth=0.5)
 
-
+    # create num_points random points and then check if they are in the circle
     for i in range(num_points):
         x = random.uniform(-1,1)
         y = random.uniform(-1,1)
@@ -33,6 +36,7 @@ def estimate_pi(num_points):
             x_outside.append(x)
             y_outside.append(y)
 
+    # do the calcultions for the estimate
     percentage_inside = circle_points / num_points
     pi_est = 4 * percentage_inside
     relative_error = abs(pi_est - math.pi) / math.pi
@@ -41,9 +45,11 @@ def estimate_pi(num_points):
 
     end = time.perf_counter()
 
+    # print the results and time taken
     print(f"Estimate: {pi_est:.6f}, Accuracy: {accuracy*100:.2f}%")
     print(f"Elapsed: {end - start:.6f} seconds")
-    
+
+    # plot the graph for a reasonable amount of points
     if num_points < 1000000:
         plt.scatter(x_inside, y_inside, color="blue", s=5)
         plt.scatter(x_outside, y_outside, color="red", s=5)
@@ -53,11 +59,8 @@ def estimate_pi(num_points):
 
     return pi_est, accuracy
 
-
-
-
-# Only runs when you run the file directly
+# calls the function only if the file is run
 if __name__ == "__main__":
-    estimate_pi(1000)   # this actually calls your function
+    estimate_pi(1000) 
 
 
